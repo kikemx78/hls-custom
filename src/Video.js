1827531eb7f5ed777bddbba6b45077d5708176b4
Enrique <edelbosque@e-sant.com>
2020-02-11 14:04:04 -0600

    v0.9

--- src/Video.js
+++ src/Video.js
@@ -12,6 +12,7 @@ export default class Video extends Component {
       videoSrc: '',
       setLevel: null,
       setVideo: false,
+      hasModal: false,
       hasCapLevel: false,
       isFullScreen: false,
       isVideoPlaying: false,
@@ -31,6 +32,7 @@ export default class Video extends Component {
     this.resetVideoUrl = this.resetVideoUrl.bind(this);
     this.updateHlsObject = this.updateHlsObject.bind(this);
     this.closeFullscreen = this.closeFullscreen.bind(this);
+    this.handleToggleModal = this.handleToggleModal.bind(this);
     
   }
 
@@ -151,10 +153,10 @@ export default class Video extends Component {
   setLevel(index) {
     console.log('set to level index ', index);
     this.setState({ setLevel: index });
-  } 
-
-  componentDidUpdate() {
-    // console.log('update video component');
+  }
+  
+  handleToggleModal() {
+    this.setState({hasModal: !this.state.hasModal});
   }
 
   render() {
@@ -198,6 +200,21 @@ export default class Video extends Component {
                 alt="play" 
                 style={{width: '20%', textAlign: 'center', transform: 'translate(0, 80%)' }} 
                 src={`data:image/svg+xml;base64, ${playIcon} `} />
+            </div>
+          }
+          {
+            this.state.hasModal && this.state.setVideo &&
+            <div 
+              onClick={this.handleToggleModal}
+              style={{ cursor: 'pointer', position: 'absolute', width: '100%', height: '100%', top: '0px', background: 'rgba(255,255,255,0.33)', zIndex: '99999999999' }}>
+                
+              <div style={{transform: 'translate(0, calc(50% - 25px))', height: '100%'}}>
+                <div style={{fontSize: '20px', position: 'relative', top: '-50px'}}>Are you still wathcing our stream ?</div>
+                <div>
+                  <span style={{borderRadius: '5px', fontSize: '20px', padding: '25px', background: 'green', position: 'relative', transform: 'translate(0, 50%)'}}>Yes</span>
+                </div>
+                <div style={{fontSize: '20px', position: 'relative', top: '50px'}}>Otherwise your session will be terminated in x seconds</div>
+              </div>
             </div>
           }
           <video
